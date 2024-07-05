4c56eea007f21a3c24584d12bec7e9eebaf87616
unknown <hsshin@euclidsoft.co.kr>
2024-07-05 17:46:45 +0900

    breadLoc project first 20240705 shs

---
+++ src/main/java/dev/bread/BreadLocProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.bread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadLocProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadLocProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bread/BreadLocProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadLocProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
